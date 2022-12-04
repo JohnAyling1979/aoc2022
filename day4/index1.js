50346197c22ff1f34220f97e6f664d5d77776112
John Ayling <john.ayling@digintent.com>
2022-12-04 09:18:14 -0600

    Add day 4.

---
+++ day4/index1.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const data = fs.readFileSync('data.txt', 'utf8').split(/\r?\n/);
+
+function fillSections(start, end) {
+	const sections = [];
+
+	for (let i = start; i <= end; i++) {
+		sections.push(i);
+	}
+
+	return sections;
+}
+
+const overlapping = data.reduce((count, row) => {
+	const [part1, part2] = row.split(',');
+
+	let [start, end] = part1.split('-');
+	let biggerSection = fillSections(+start, +end);
+
+	[start, end] = part2.split('-');
+	let smallerSection = fillSections(+start, +end);
+
+	if (biggerSection.length < smallerSection.length) {
+		const temp = biggerSection;
+		biggerSection = smallerSection;
+		smallerSection = temp;
+	}
+
+	const remaining = smallerSection.filter(item => !biggerSection.includes(item));
+
+	if (remaining.length === 0) {
+		count++;
+	}
+
+	return count;
+}, 0);
+
+console.log(overlapping);
