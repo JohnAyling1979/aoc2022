e1acf846fdcaf593a82ecc890ff09a5093351c42
John Ayling <john.ayling@digintent.com>
2022-12-17 07:42:52 -0600

    Refactor day 1 and add tests.

---
+++ day1/day1.test.js
@@ -0,0 +1,14 @@
+const {part1, part2} = require('.');
+
+describe('Day 1: Calorie Counting', () => {
+	describe('Sample Data', () => {
+		test('Part 1 total calories should be 24000', () => {expect(part1('sample.txt')).toBe(24000)});
+		test('Part 2 total calories should be 45000', () => {expect(part2('sample.txt')).toBe(45000)});
+	});
+
+	describe('Actual Data', () => {
+		test('Part 1 total calories should be 69626', () => {expect(part1('data.txt')).toBe(69626)});
+		test('Part 2 total calories should be 206780', () => {expect(part2('data.txt')).toBe(206780)});
+	});
+});
+
---
+++ day1/index.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+function part1(dataFile) {
+	const data = fs.readFileSync(`${__dirname}/${dataFile}`, 'utf8').split(/\r?\n/);
+
+	const elvies = [];
+	let index = 0;
+
+	elvies[0] = 0;
+
+	data.forEach(line => {
+		if (line === '') {
+			index++
+			elvies[index] = 0;
+		} else {
+			elvies[index] += +line
+		}
+	})
+
+	elvies.sort((a, b) => b - a);
+
+	return elvies[0];
+}
+
+function part2(dataFile) {
+	const data = fs.readFileSync(`${__dirname}/${dataFile}`, 'utf8').split(/\r?\n/);
+
+	const elvies = [];
+	let index = 0;
+
+	elvies[0] = 0;
+
+	data.forEach(line => {
+		if (line === '') {
+			index++
+			elvies[index] = 0;
+		} else {
+			elvies[index] += +line
+		}
+	})
+
+	elvies.sort((a, b) => b - a);
+
+	return elvies[0] + elvies[1] + elvies[2];
+}
+
+console.log(part1('data.txt'));
+console.log(part2('data.txt'));
+
+module.exports = {
+	part1,
+	part2
+};
