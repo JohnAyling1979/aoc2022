9af58b32ef3aaeb0fc6896d9711c740aa32e26ab
John Ayling <john.ayling@digintent.com>
2022-12-17 08:07:25 -0600

    Update template.

---
+++ day-template/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+function part1(dataFile) {
+	const data = fs.readFileSync(`${__dirname}/${dataFile}`, 'utf8').split(/\r?\n/);
+
+	return null;
+}
+
+function part2(dataFile) {
+	const data = fs.readFileSync(`${__dirname}/${dataFile}`, 'utf8').split(/\r?\n/);
+
+	return null;
+}
+
+console.log(part1('sample.txt'));
+console.log(part2('sample.txt'));
+
+module.exports = {
+	part1,
+	part2
+};
--- day-template/index1.js
+++
@@ -1,2 +0,0 @@
-const fs = require('fs');
-const data = fs.readFileSync('sample.txt', 'utf8').split(/\r?\n/);
